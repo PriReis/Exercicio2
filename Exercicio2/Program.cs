d3ec9a635ce243979ab4ee26af596f840ccc02c6
prisc <prisc@LAPTOP-KQ166T54>
2023-03-10 12:56:36 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Exercicio 2
+//a)Desenvolvam um programa que armazene uma lista de convidados utilizando um array
+
+using System;
+using System.Collections.Generic;
+public class Exerc2A
+{
+    public static void Main()
+    {
+        string[] convidados = new string[6];
+
+        convidados[0] = "João Gabriel";
+        convidados[1] = "Agnes";
+        convidados[2] = "Priscila Valentin";
+        convidados[3] = "Caue";
+        convidados[4] = "Evandro";
+        convidados[5] = "Camila";
+
+        Console.WriteLine("O número de convidados é igual a " + convidados.Length);
+
+    }
+}
